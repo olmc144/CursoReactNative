ece9c97aae81250ff8226ba9b507920a285fc619
Oscar Luis Martinez C <oscar.martinez@imedicalservices.com>
2022-11-26 15:11:56 -0500

    instalacion de paquetes nuevos para navegación e importació

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import LoginForm from "./src/components/LoginForm";
 import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Saludar />
-      <LoginForm />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Saludar />
+        <LoginForm />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
